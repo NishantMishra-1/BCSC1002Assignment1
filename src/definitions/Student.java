c347d516e4d5a4b2fe3753f797f8fcf6f312300c
My name <nishant.mishra_cs19@gla.ac.in>
2020-08-30 17:25:48 +0530

    add tostring, equals  and hascode method to Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,6 +7,9 @@
 package definitions;
 
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Student {
     private String[] studentName;
     private long universityRollNumber;
@@ -50,5 +53,52 @@ public class Student {
     public void setIssuedBooks(Book[] issuedBooks) {
         this.issuedBooks = issuedBooks;
     }
+
+    /**
+     * This method issue book to a student.
+     *
+     * @param issuedBook book has to be issued to a student.
+     */
+    public void issueBook(Book issuedBook) {
+        System.out.println("Book is issued to student");
+    }
+
+    /**
+     * This method checks issued book record and returns the issued book back to library
+     *
+     * @param bookReturn The issued book returned.
+     * @return a book to be returned.
+     */
+    public Book returnIssuedBook(String bookReturn) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Student Name- " + Arrays.toString(studentName) +
+                "   University Roll Number- " + universityRollNumber +
+                "  Number Of Books Issued- " + numberOfBooksIssued +
+                "  Issued Books- " + Arrays.toString(issuedBooks);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return universityRollNumber == student.universityRollNumber &&
+                numberOfBooksIssued == student.numberOfBooksIssued &&
+                Arrays.equals(studentName, student.studentName) &&
+                Arrays.equals(issuedBooks, student.issuedBooks);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(universityRollNumber, numberOfBooksIssued);
+        result = 31 * result + Arrays.hashCode(studentName);
+        result = 31 * result + Arrays.hashCode(issuedBooks);
+        return result;
+    }
+
 
 }
