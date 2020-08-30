935c11b299b5211f31ab3a5f17e6303d214280d9
My name <nishant.mishra_cs19@gla.ac.in>
2020-08-30 15:49:07 +0530

    add constructor to book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -5,11 +5,17 @@
  *  File Name : Book.java
  * */
 package definitions;
-import java.util.Objects;
+
 public class Book {
-        private String bookName;
-        private String authorName;
-        private String ISBNNumberOfBook;
+    private String bookName;
+    private String authorName;
+    private String ISBNNumberOfBook;
 
-
+    public Book(String bookName, String authorName, String ISBNNumberOfBook) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.ISBNNumberOfBook = ISBNNumberOfBook;
     }
+
+
+}
